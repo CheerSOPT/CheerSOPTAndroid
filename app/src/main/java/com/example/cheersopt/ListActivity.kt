28f2f61912b05840f291c2ba7765c3e735cc25c9
mdb1217 <mdb1217@naver.com>
2020-11-22 07:58:24 +0900

    [Create UI]list final

--- app/src/main/java/com/example/cheersopt/ListActivity.kt
+++ app/src/main/java/com/example/cheersopt/ListActivity.kt
@@ -29,10 +29,13 @@ class ListActivity : AppCompatActivity() {
         recyclerView.layoutManager = staggeredGridLayoutManager
 
         recyclerAdapter.data = mutableListOf(
-            DrinkData("다빈주", "소주3\n맥주1"),
-            DrinkData("다빈주", "소주3\n맥주1\n사이다1"),
-            DrinkData("다빈주", "소주3\n맥주1"),
-            DrinkData("다빈주", "소주3\n맥주3")
+            DrinkData("현주야 한 번\n 더 가자", "토닉5"),
+            DrinkData("치얼솝트 1위\n 축하주", "소주2\n토닉3"),
+            DrinkData("혜인아 운\n좋다^^", "청하3\n토닉2"),
+            DrinkData("아...약해 \n약해....", "맥주3\n콜라2"),
+            DrinkData("오늘 구디 희영이\n가 점령", "소주1\n사이다4"),
+            DrinkData("치얼솝트 리액션왕\n 예나", "복분자4\n소주1"),
+            DrinkData("지리는 이기상\n 첫 회식", "복분자2\n청하2\n사이다1")
         )
         recyclerAdapter.notifyDataSetChanged()
     }
