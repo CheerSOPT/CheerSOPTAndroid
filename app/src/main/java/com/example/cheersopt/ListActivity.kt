ea41ce407e64be9d21fa6ee1160bfe6a5d36d87d
mdb1217 <mdb1217@naver.com>
2020-11-22 05:57:48 +0900

    [Implement Features] listview

--- app/src/main/java/com/example/cheersopt/ListActivity.kt
+++ app/src/main/java/com/example/cheersopt/ListActivity.kt
@@ -1,7 +1,9 @@
 package com.example.cheersopt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -11,6 +13,12 @@ class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        val main_btn = findViewById<TextView>(R.id.main_btn)
+        main_btn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
         recyclerAdapter = RecyclerAdapter(this)
 
